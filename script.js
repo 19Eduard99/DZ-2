25e347715644962ebdad76c82edd92dea136da11
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-02-28 23:33:48 +0200

    add1

---
+++ script.js
@@ -0,0 +1,21 @@
+const a = parseInt(prompt("a = ",));
+const b = parseInt(prompt("b = ",));
+function sum(a, b) {
+    return a + b;
+}
+alert("Result: " + sum(a,b));
+
+function minus(a, b){
+    return a - b;
+}
+alert("Result: " + minus(a,b));
+
+function multiply(a, b){
+    return a * b;
+}
+alert("Result: " + multiply(a,b));
+
+function divide(a, b){
+    return a / b;
+}
+alert("Result: " + divide(a,b));
